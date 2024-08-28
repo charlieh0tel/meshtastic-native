3e05dcb536821ccd52e52cf87428698b11efab77
Christopher Hoover <ch@murgatroid.com>
2024-08-27 20:33:57 -0700

    .

--- setup.sh
+++ setup.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
 set -o errexit
+set -o nounset
+set -o xtrace
+
+python3 -m venv .
+
+. bin/activate
+
+pip install meshtastic
+pip install -U platformio
 
 git clone https://github.com/meshtastic/firmware.git
 cd firmware
